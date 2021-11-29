8f5b2d62c1e61236833f9911c8fc39a9fbcd7c6f
Kavyashree <kavya27.devadiga@gmail.com>
2021-11-29 02:10:38 -0500

    Resolve issue: robot moving in circles

--- src/move.cpp
+++ src/move.cpp
@@ -42,92 +42,75 @@
 #include "sensor_msgs/LaserScan.h"
 
 Move::Move(ros::NodeHandle nh) {
-  ROS_INFO_STREAM("Initialized node.");
-  ROS_DEBUG_STREAM("Move object created.");
-  send_velocity = nh.advertise<geometry_msgs::Twist>
-    ("/mobile_base/commands/velocity", 150);
-  turtle_vel = 150;
-  laser_scan = nh.subscribe("scan", 50, &Move::getLaserData, this);
+    ROS_INFO_STREAM("Initialized node.");
+    ROS_DEBUG_STREAM("Move object created.");
+    send_velocity = nh.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 150);
+    turtle_vel = 150;
+    laser_scan = nh.subscribe("scan", 50, &Move::getLaserData, this);
 }
 
-
-void Move::move_() {
-  if (scan.collisionyn() == true) {
-    robot_vel.linear.x = 0.0;
-    robot_vel.angular.z = 1.0;
-  } else {
-    robot_vel.linear.x = 0.5;
-    robot_vel.angular.z = 0.0;
-  }
-  turtle_vel_.publish(robot_vel);
-}
-
-
-void Move::getLaserData(const sensor_msgs::LaserScan::ConstPtr &laser_data) {
+void Move::getLaserData(const sensor_msgs::LaserScan::ConstPtr &laser_data) {    
     move_yn = true;
-    ROS_INFO_STREAM("Turtlebot Walking.");
-    for (int i = 0; i < laser_data->ranges.size(); i++) {
-        if (laser_data->ranges[i] < 1.0) {
-            turn_yn = true;
-            move_yn = false;
-            ROS_DEBUG_STREAM("Lase value: " << laser_data->ranges[i]);
+    auto start_ = laser_data->ranges.begin() ;
+    auto end_ = laser_data->ranges.end() - 30;
+    std::vector<double> collision_;
+    std::vector<double> laser_range;
+
+    collision_ = std::vector<double>(end_,end_+30);
+    laser_range = std::vector<double>(start_, start_+30);
+
+    collision_.insert(collision_.end(), laser_range.begin(),
+                              laser_range.end());
+    for (auto &range : collision_)  {
+        if (range < 0.4) {
+            ROS_DEBUG_STREAM("Lase value: " << range);
             ROS_INFO_STREAM("Obstacle close");
-            ros::Duration(1.0).sleep();
-            return;
-        } else {
-            ROS_INFO_STREAM("Obstacle dodged");
-            turn_yn = false;
-            move_yn = true;
+            collision_yn = true;
+            move_yn = false;
         }
+        ROS_INFO_STREAM("Path clear.");
     }
-    collision_yn = false;
-    ROS_DEBUG_STREAM("Path clear.");
 }
 
 void Move::stopMoving(ros::Publisher turtle_vel_) {
-  geometry_msgs::Twist robot_vel;
-  robot_vel.linear.x = 0.0;
-  robot_vel.linear.y = 0.0;
-  robot_vel.linear.z = 0.0;
-  robot_vel.angular.x = 0.0;
-  robot_vel.angular.y = 0.0;
-  robot_vel.angular.z = 0.0;
-  turtle_vel_.publish(robot_vel);
+    robot_vel.linear.x = 0.0;
+    robot_vel.linear.y = 0.0;
+    robot_vel.linear.z = 0.0;
+    robot_vel.angular.x = 0.0;
+    robot_vel.angular.y = 0.0;
+    robot_vel.angular.z = 0.0;
+    turtle_vel_.publish(robot_vel);
 }
 
-void Move::turnBot(ros::NodeHandle nh, ros::Publisher turtle_vel_) {  
-  ROS_INFO_STREAM("Turning Robot.");
-  ros::Rate publish_rate(15);
-  geometry_msgs::Twist robot_vel;
-  stopMoving(turtle_vel_);
-  robot_vel.angular.z = 0.5;
-  turtle_vel_.publish(robot_vel);
-  while (!move_yn) {
-    publish_rate.sleep();
-    ros::spinOnce();
-  }
-  stopMoving(turtle_vel_);
-  turn_yn = false;
+void Move::turnBot(ros::NodeHandle nh, ros::Publisher turtle_vel_) {
+    ROS_INFO_STREAM("Turning Robot.");
+    robot_vel.angular.z = 0.3;
+    turtle_vel_.publish(robot_vel);
+    ros::Rate publish_rate(10);
+    while (!move_yn) {
+        ROS_DEBUG_STREAM("Move ahead flag false.");
+        publish_rate.sleep();
+        ros::spinOnce();
+    }
+    stopMoving(turtle_vel_);
+    collision_yn = false;
 }
 
 void Move::startMoving(ros::NodeHandle nh, ros::Publisher turtle_vel_,
-  ros::Rate publish_rate) {
-  ROS_INFO_STREAM("Robot is moving.");
-  geometry_msgs::Twist robot_vel;
-  while (ros::ok()) {
-    if (Move::turn_yn) {
-      Move::stopMoving(turtle_vel_);
-      ROS_INFO_STREAM("Obstacle ahead.");
-      turnBot(nh, turtle_vel_);
-    } else {
-      stopMoving(turtle_vel_);
-      robot_vel.linear.x = 0.3;
-    }
-    turtle_vel_.publish(robot_vel);
-    ros::spinOnce();
-    ros::Duration(1.0).sleep();
-  }
+                       ros::Rate publish_rate) {    
+        ROS_INFO_STREAM("Robot walking randomly.");
+        if (collision_yn) {
+            stopMoving(turtle_vel_);
+            turnBot(nh, turtle_vel_);
+        }  else {
+            robot_vel.linear.x = 0.3;
+            robot_vel.angular.z = 0.0;
+        }
+        turtle_vel_.publish(robot_vel);
+        ros::spinOnce();
+        publish_rate.sleep();
+        ROS_INFO_STREAM("Velocity:" << robot_vel.linear.x );
+        ROS_INFO_STREAM(" and " << robot_vel.angular.z);
 }
-
 
 Move::~Move() {}
